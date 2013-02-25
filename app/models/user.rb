b5e1e5e0d0114414b9cf84692b0d985cbbb6eafa
hoangth55 <hoangth_55@vnu.edu.vn>
2013-02-25 16:23:48 +0700

    Editting error messages in model.user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, 
   					format: { with: VALID_EMAIL_REGEX }, 
   					uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
+
+  validates_presence_of :password, :message => 'TYPE ANYTHING THAT YOU WANT TO HEAR'
 end
