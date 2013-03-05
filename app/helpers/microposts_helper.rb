bbcd88ac7453730a9ec8d239081815b89d1258c5
hoangth55 <hoangth_55@vnu.edu.vn>
2013-03-05 14:23:52 +0700

    Elomonate duplicatation using micropost_helper.rb

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,2 +1,7 @@
 module MicropostsHelper
+	def delete_link_for(polymorphic_object)
+	    if current_user?(polymorphic_object.user) 
+	      	link_to "delete", polymorphic_object, method: :delete, data: { confirm: "You sure?" },title: polymorphic_object.content 
+        end
+  	end
 end
